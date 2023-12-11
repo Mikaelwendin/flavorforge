979cef2ae2f90a780827dc1f887fcb271af0b825
Mikaelwendin <mikael.wendin83@gmail.com>
2023-12-11 19:29:32 +0100

    basic API-call done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire/compat';
 import { FirestoreModule } from '@angular/fire/firestore';
 import { FormsModule } from '@angular/forms';
@@ -11,10 +12,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { RegisterComponent } from './register/register.component';
 import { UserEffects } from './user/user.effects';
 import { userReducer } from './user/user.reducer';
-import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { NavigationComponent } from './navigation/navigation.component';
     EffectsModule.forRoot([]),
     StoreModule.forRoot({ user: userReducer }),
     EffectsModule.forRoot([UserEffects]),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent {
+  searchQuery: string = '';
+  searchResults: any[] = [];
 
+  constructor(private recipeService: RecipeService) {}
+
+  searchRecipes() {
+    if (this.searchQuery.trim() !== '') {
+      this.recipeService.searchRecipes(this.searchQuery).subscribe(
+        (response) => {
+          this.searchResults = response.meals;
+        },
+        (error) => {
+          console.error('Error when searching', error);
+        }
+      );
+    }
+  }
 }
