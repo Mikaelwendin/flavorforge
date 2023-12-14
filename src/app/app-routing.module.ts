392990481ef64f4b1355e7a910d991f9d54ae6de
Mikaelwendin <mikael.wendin83@gmail.com>
2023-12-14 14:07:06 +0100

    added my page component and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
+import { MyPageComponent } from './my-page/my-page.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'landing', component: LandingComponent },
+  { path: 'my-page', component: MyPageComponent },
   { path: 'recipe/:id', component: RecipeDetailComponent },
 ];
 
