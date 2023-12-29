b9fed70f6956b11be0c38eb58017c674b7bfe773
Mikaelwendin <mikael.wendin83@gmail.com>
2023-12-29 12:48:58 +0100

    reworked some aspects of login and registration for better user feedback and errorhandling AGAIN

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -32,31 +32,33 @@ export class AuthService {
       .subscribe();
   }
 
-  register(email: string, password: string, username: string): Promise<any> {
-    return this.afAuth
-      .createUserWithEmailAndPassword(email, password)
-      .then((credential) => {
-        const uid = credential.user?.uid;
-        console.log('Updating profile with username:', username);
-        return credential.user
-          ?.updateProfile({ displayName: username })
-          .then(() => {
-            console.log('Profile updated successfully');
-            return this.firestore.collection('users').doc(uid).set({
-              uid: uid,
-              email: email,
-              username: username,
-              favoriteRecipes: [],
-            });
-          })
-          .then(() => {
-            return credential;
-          });
-      })
-      .catch((error) => {
-        console.error('Error during registration:', error);
-        throw error;
+  async register(
+    email: string,
+    password: string,
+    username: string
+  ): Promise<any> {
+    try {
+      const credential = await this.afAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      const uid = credential.user?.uid;
+
+      console.log('Updating profile with username:', username);
+      await credential.user?.updateProfile({ displayName: username });
+
+      console.log('Profile updated successfully');
+      await this.firestore.collection('users').doc(uid).set({
+        uid: uid,
+        email: email,
+        username: username,
+        favoriteRecipes: [],
       });
+
+      return credential;
+    } catch (error) {
+      throw this.handleAuthError(error);
+    }
   }
 
   async login(email: string, password: string): Promise<any> {
@@ -69,9 +71,7 @@ export class AuthService {
       console.log('Successfully signed in:', user?.displayName);
       return user;
     } catch (error) {
-      const firebaseError = error as FirebaseError;
-      console.warn('Login error:', firebaseError.message);
-      throw firebaseError;
+      throw this.handleAuthError(error);
     }
   }
 
@@ -80,10 +80,30 @@ export class AuthService {
       this.isAuthenticatedSubject.next(false);
     });
   }
+
   isLoggedIn(): boolean {
     return this.isAuthenticatedSubject.value;
   }
+
   getCurrentUser(): firebase.default.User | null {
     return this.currentUserSubject.value;
+  }
+
+  private handleAuthError(error: any): Error {
+    const firebaseError = error as FirebaseError;
+    switch (firebaseError.code) {
+      case 'auth/user-not-found':
+        return new Error(
+          'User not found. Please check your email and try again.'
+        );
+      case 'auth/wrong-password':
+        return new Error('Invalid password. Please try again.');
+      case 'auth/email-already-in-use':
+        return new Error(
+          'Email is already in use. Please use a different email.'
+        );
+      default:
+        return new Error('Authentication failed. Please try again later.');
+    }
   }
 }
